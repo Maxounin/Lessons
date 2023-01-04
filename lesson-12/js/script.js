e5dd7ef370b67bfab9adf0068926c2644a8bf271
Maxounin <118292365+Maxounin@users.noreply.github.com>
2023-01-04 05:09:19 +0300

    done

---
+++ lesson-12/js/script.js
@@ -0,0 +1,5 @@
+let firstName = prompt('Введите ваше имя');
+let lastName = prompt('Введите вашу фамилию');
+let gender = prompt('Укажите ваш пол');
+let age = prompt('Введите ваш возраст');
+alert(`Имя: ${firstName} \nФамилия: ${lastName} \nПол: ${gender} \nВозраст: ${age}`);
