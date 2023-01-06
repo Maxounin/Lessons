8079e256a8551a0f934a979a210d652453c2c107
Maxounin <118292365+Maxounin@users.noreply.github.com>
2023-01-06 11:43:18 +0300

    add output from prompts to div

--- lesson-12/js/script.js
+++ lesson-12/js/script.js
@@ -3,3 +3,7 @@ let lastName = prompt('Введите вашу фамилию');
 let gender = prompt('Укажите ваш пол');
 let age = prompt('Введите ваш возраст');
 alert(`Имя: ${firstName} \nФамилия: ${lastName} \nПол: ${gender} \nВозраст: ${age}`);
+document.getElementById('firstName').textContent = firstName;
+document.getElementById('lastName').textContent = lastName;
+document.getElementById('gender').textContent = gender;
+document.getElementById('age').textContent = age;
