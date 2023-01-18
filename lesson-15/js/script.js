2ecf2a827a8456c4186899a909897c3475b58fd3
Maxounin <118292365+Maxounin@users.noreply.github.com>
2023-01-18 13:27:47 +0300

    almost done

---
+++ lesson-15/js/script.js
@@ -0,0 +1,27 @@
+let sum = 0;
+
+function sumInput() {
+    
+    let arr = [];
+    let i;
+
+    do {
+        i = prompt('enter');
+        arr.push(i);
+    } while(!(i === "" || i === null));
+
+    //for (let i = 0; i < arr.length; i++) {
+    //    sum += +arr[i];
+    //}
+
+    for (let  of arr) {
+        
+    }
+    
+}
+
+sumInput();
+
+
+
+console.log(sum);
